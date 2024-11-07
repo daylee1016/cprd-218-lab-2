abc29af4bdf38c7512336f3eed9090027fb6ccb8
daylee1016 <dayeong.lee@edu.sait.ca>
2024-11-06 17:05:11 -0700

    cprg-218-lab-2
    cprg 218 lab 2

---
+++ lab2.js
@@ -0,0 +1,20 @@
+lab2.addEventListener("click",function(){
+})
+
+BighornSheep.addEventListener("click",function(){
+    pagetitle.innerHTML="bighornSheep";
+	document.getElementById('BighornSheep').src="sheep.webp".style.display="inital";
+    document.getElementById('blackbear').src="bear.webp".style.display="none";
+})
+
+BlackBear.addEventListener("click",function(){
+    pagetitle.innerHTML="BlackBear";
+	document.getElementById('blackbear').src="bear.webp";
+
+    
+})
+
+BlackBear.addEventListener("click",function(){
+    pagetitle.innerHTML="Moose";
+	document.getElementById('Moose').src="moose.webp";
+})
